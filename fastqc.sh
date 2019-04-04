4c7816634525d0b490fe89496a24a2eba868cab2
CarolinaPB <carolina.pitabarros@gmail.com>
2019-04-04 15:08:45 +0200

    commented lines

--- fastqc.sh
+++ fastqc.sh
@@ -1,11 +1,11 @@
-#!/bin/bash -l
-SBATCH -A g2019003_1
-SBATCH -p core
-SBATCH -n 2
-SBATCH -t 00:05:00
-SBATCH -J run_fastqc
-SBATCH --mail-type=ALL
-SBATCH --mail-user carolina.pitabarros@gmail.com
+#!/bin/bash -L
+#SBATCH -A g2019003
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 00:05:00
+#SBATCH -J run_fastqc
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user carolina.pitabarros@gmail.com
 # Load modules
 module load bioinfo-tools
 module load FastQC/0.11.5
