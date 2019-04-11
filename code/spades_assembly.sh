b833e91c3f19d4277933a37572b0b6c8a0fb9f2c
CarolinaPB <carolina.pitabarros@gmail.com>
2019-04-11 16:22:17 +0200

    changed version of spades in module load and running time

--- code/spades_assembly.sh
+++ code/spades_assembly.sh
@@ -2,12 +2,12 @@
 #SBATCH -A g2019003
 #SBATCH -p core
 #SBATCH -n 6
-#SBATCH -t 220:00:00
+#SBATCH -t 200:00:00
 #SBATCH -J spades
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user Carolina.Barros.8371@student.uu.se
 # Load modules
 module load bioinfo-tools
-module load spades
+module load spades/3.12.0
 # Your commands
 spades.py --mp-1 /home/carolpb/GenomeAnalysis/data/wgs/sel2_SRR5819794.trim_1P.fastq.gz --mp-2 /home/carolpb/GenomeAnalysis/data/wgs/sel2_SRR5819794.trim_2P.fastq.gz --mp-1 /home/carolpb/GenomeAnalysis/data/wgs/sel2_SRR5819795.trim_1P.fastq.gz --mp-2 /home/carolpb/GenomeAnalysis/data/wgs/sel2_SRR5819795.trim_2P.fastq.gz --mp-1 /home/carolpb/GenomeAnalysis/data/wgs/sel2_SRR5819796.trim_1P.fastq.gz --mp-2 /home/carolpb/GenomeAnalysis/data/wgs/sel2_SRR5819796.trim_2P.fastq.gz --mp-1 /home/carolpb/GenomeAnalysis/data/wgs/sel2_SRR5819797.trim_1P.fastq.gz --mp-2 /home/carolpb/GenomeAnalysis/data/wgs/sel2_SRR5819797.trim_2P.fastq.gz --mp-1 /home/carolpb/GenomeAnalysis/data/wgs/sel2_SRR5819798.trim_1P.fastq.gz --mp-2 /home/carolpb/GenomeAnalysis/data/wgs/sel2_SRR5819798.trim_2P.fastq.gz --mp-1 /home/carolpb/GenomeAnalysis/data/wgs/sel2_SRR5819799.trim_1P.fastq.gz --mp-2 /home/carolpb/GenomeAnalysis/data/wgs/sel2_SRR5819799.trim_2P.fastq.gz --pe-1 /home/carolpb/GenomeAnalysis/data/wgs/sel2_SRR5819794.trim_1U.fastq.gz --pe-2 /home/carolpb/GenomeAnalysis/data/wgs/sel2_SRR5819794.trim_2U.fastq.gz --pe-1 /home/carolpb/GenomeAnalysis/data/wgs/sel2_SRR5819795.trim_1U.fastq.gz --pe-2 /home/carolpb/GenomeAnalysis/data/wgs/sel2_SRR5819795.trim_2U.fastq.gz --pe-1 /home/carolpb/GenomeAnalysis/data/wgs/sel2_SRR5819796.trim_1U.fastq.gz --pe-2 /home/carolpb/GenomeAnalysis/data/wgs/sel2_SRR5819796.trim_2U.fastq.gz --pe-1 /home/carolpb/GenomeAnalysis/data/wgs/sel2_SRR5819797.trim_1U.fastq.gz --pe-2 /home/carolpb/GenomeAnalysis/data/wgs/sel2_SRR5819797.trim_2U.fastq.gz --pe-1 /home/carolpb/GenomeAnalysis/data/wgs/sel2_SRR5819798.trim_1U.fastq.gz --pe-2 /home/carolpb/GenomeAnalysis/data/wgs/sel2_SRR5819798.trim_2U.fastq.gz --pe-1 /home/carolpb/GenomeAnalysis/data/wgs/sel2_SRR5819799.trim_1U.fastq.gz --pe-2 /home/carolpb/GenomeAnalysis/data/wgs/sel2_SRR5819799.trim_2U.fastq.gz -o /home/carolpb/GenomeAnalysis/results/3_Assembly_spades
