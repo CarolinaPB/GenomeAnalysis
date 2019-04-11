66e1d5b5e1c7ec5b90a1019b02dcab5ae01b3b0d
CarolinaPB <carolina.pitabarros@gmail.com>
2019-04-11 17:15:57 +0200

    changed output directory and email

--- code/soapdenovo_assembly_bash.sh
+++ code/soapdenovo_assembly_bash.sh
@@ -6,10 +6,10 @@
 #SBATCH --ntasks-per-node=4
 #SBATCH -J soapdenovo_assembly
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user carolina.pitabarros@gmail.com
+#SBATCH --mail-user Carolina.Barros.8371@student.uu.se
 # Load modules
 module load bioinfo-tools
 module load soapdenovo-soapdenovo/2.04-r240
 
 
-SOAPdenovo-63mer all -s /home/carolpb/GenomeAnalysis/code/soapdenovo_assembly_config.txt -K 49 -o /home/carolpb/GenomeAnalysis/results/2_Assembly_dna/output49 -p $SLURM_NTASKS_PER_NODE
+SOAPdenovo-63mer all -s /home/carolpb/GenomeAnalysis/code/soapdenovo_assembly_config.txt -K 49 -o /home/carolpb/GenomeAnalysis/results/3_Assembly_soapdenovo_dna/output49 -p $SLURM_NTASKS_PER_NODE
