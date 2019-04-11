60b181d09f8e801e0b72dc7a518aec19f86865e5
CarolinaPB <carolina.pitabarros@gmail.com>
2019-04-11 16:07:15 +0200

    changed run time

--- code/spades_assembly.sh
+++ code/spades_assembly.sh
@@ -2,7 +2,7 @@
 #SBATCH -A g2019003
 #SBATCH -p core
 #SBATCH -n 2
-#SBATCH -t 00:30:00
+#SBATCH -t 03:00:00
 #SBATCH -J spades
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user Carolina.Barros.8371@student.uu.se
