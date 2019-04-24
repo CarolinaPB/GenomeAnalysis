0bf391a4501b9597b25cbae25ec893493d7cc179
CarolinaPB <carolina.pitabarros@gmail.com>
2019-04-24 08:59:49 +0200

    one file for paired end and one for single

--- code/trinity_sbatch.sh
+++ code/trinity_sbatch.sh
@@ -48,39 +48,4 @@ module load trinity
 /home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719214.trim_2P.fastq.gz,\
 /home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719241.trim_2P.fastq.gz,\
 /home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719242.trim_2P.fastq.gz \
---single \
-/home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719013.trim_1U.fastq.gz,\
-/home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719014.trim_1U.fastq.gz,\
-/home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719015.trim_1U.fastq.gz,\
-/home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719016.trim_1U.fastq.gz,\
-/home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719017.trim_1U.fastq.gz,\
-/home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719018.trim_1U.fastq.gz,\
-/home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719204.trim_1U.fastq.gz,\
-/home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719206.trim_1U.fastq.gz,\
-/home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719207.trim_1U.fastq.gz,\
-/home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719208.trim_1U.fastq.gz,\
-/home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719209.trim_1U.fastq.gz,\
-/home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719211.trim_1U.fastq.gz,\
-/home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719212.trim_1U.fastq.gz,\
-/home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719213.trim_1U.fastq.gz,\
-/home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719214.trim_1U.fastq.gz,\
-/home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719241.trim_1U.fastq.gz,\
-/home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719242.trim_1U.fastq.gz,\
-/home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719013.trim_2U.fastq.gz,\
-/home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719014.trim_2U.fastq.gz,\
-/home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719015.trim_2U.fastq.gz,\
-/home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719016.trim_2U.fastq.gz,\
-/home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719017.trim_2U.fastq.gz,\
-/home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719018.trim_2U.fastq.gz,\
-/home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719204.trim_2U.fastq.gz,\
-/home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719206.trim_2U.fastq.gz,\
-/home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719207.trim_2U.fastq.gz,\
-/home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719208.trim_2U.fastq.gz,\
-/home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719209.trim_2U.fastq.gz,\
-/home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719211.trim_2U.fastq.gz,\
-/home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719212.trim_2U.fastq.gz,\
-/home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719213.trim_2U.fastq.gz,\
-/home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719214.trim_2U.fastq.gz,\
-/home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719241.trim_2U.fastq.gz,\
-/home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719242.trim_2U.fastq.gz \
---output /home/carolpb/GenomeAnalysis/results/4_Transcriptome_Assembly_Trinity
+--output /home/carolpb/GenomeAnalysis/results/4_Transcriptome_Assembly_Trinity/Paired
---
+++ code/trinity_sbatch_single.sh
@@ -0,0 +1,50 @@
+#!/bin/bash -l
+#SBATCH -A g2019003
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 04:00:00
+#SBATCH -J trinity
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user Carolina.Barros.8371@student.uu.se
+
+# Load modules
+module load bioinfo-tools
+module load trinity
+
+/sw/bioinfo/trinity/2.4.0/rackham/Trinity --seqType fq --max_memory 13.6G \
+--single \
+/home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719013.trim_1U.fastq.gz,\
+/home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719014.trim_1U.fastq.gz,\
+/home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719015.trim_1U.fastq.gz,\
+/home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719016.trim_1U.fastq.gz,\
+/home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719017.trim_1U.fastq.gz,\
+/home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719018.trim_1U.fastq.gz,\
+/home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719204.trim_1U.fastq.gz,\
+/home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719206.trim_1U.fastq.gz,\
+/home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719207.trim_1U.fastq.gz,\
+/home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719208.trim_1U.fastq.gz,\
+/home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719209.trim_1U.fastq.gz,\
+/home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719211.trim_1U.fastq.gz,\
+/home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719212.trim_1U.fastq.gz,\
+/home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719213.trim_1U.fastq.gz,\
+/home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719214.trim_1U.fastq.gz,\
+/home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719241.trim_1U.fastq.gz,\
+/home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719242.trim_1U.fastq.gz,\
+/home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719013.trim_2U.fastq.gz,\
+/home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719014.trim_2U.fastq.gz,\
+/home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719015.trim_2U.fastq.gz,\
+/home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719016.trim_2U.fastq.gz,\
+/home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719017.trim_2U.fastq.gz,\
+/home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719018.trim_2U.fastq.gz,\
+/home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719204.trim_2U.fastq.gz,\
+/home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719206.trim_2U.fastq.gz,\
+/home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719207.trim_2U.fastq.gz,\
+/home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719208.trim_2U.fastq.gz,\
+/home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719209.trim_2U.fastq.gz,\
+/home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719211.trim_2U.fastq.gz,\
+/home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719212.trim_2U.fastq.gz,\
+/home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719213.trim_2U.fastq.gz,\
+/home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719214.trim_2U.fastq.gz,\
+/home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719241.trim_2U.fastq.gz,\
+/home/carolpb/GenomeAnalysis/data/rna/pre_trimmed/sel3_SRR1719242.trim_2U.fastq.gz \
+--output /home/carolpb/GenomeAnalysis/results/4_Transcriptome_Assembly_Trinity/Single
