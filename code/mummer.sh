be6358537baeefb0fd580836b009ae47ad30c007
CarolinaPB <carolina.pitabarros@gmail.com>
2019-04-25 10:00:11 +0200

    mummer runs on local lab computer

--- code/mummer.sh
+++ code/mummer.sh
@@ -1,14 +1,3 @@
-#!/bin/bash -l
-#SBATCH -A g2019003
-#SBATCH -p core
-#SBATCH -n 2
-#SBATCH -t 00:30:00
-#SBATCH -J run_mummer
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user carolina.pitabarros@gmail.com
-# Load modules
-module load bioinfo-tools
-module load MUMmer
-# Your commands
+#Run locally on the lab computers
 
-mummer -mum -b -c /home/carolpb/GenomeAnalysis/data/wgs_from_paper/sel2_NW_015504334.fna.gz /home/carolpb/GenomeAnalysis/results/2_GapCloser_dna/output_gapcloser.fasta /home/carolpb/GenomeAnalysis/results/3_Assembly_spades/scaffolds.fasta > /home/carolpb/GenomeAnalysis/results/5_Result_Mummer/output_mummer.mums
+/usr/local/MUMmer3.23/mummer -mum -b -c /home/caromoit/Desktop/sel2_NW_015504334.fna.gz //home/caromoit/Desktop/output_gapcloser.fasta /home/caromoit/Desktop/scaffolds.fasta > /home/caromoit/Desktop/output_mummer.mums
