a7a6f6f372cd669a2667e94ec21d57c9ad637fa0
CarolinaPB <35965407+CarolinaPB@users.noreply.github.com>
2019-05-02 16:41:59 +0200

    Update mummer.sh

--- code/mummer.sh
+++ code/mummer.sh
@@ -1,3 +1,26 @@
-#Run locally on the lab computers
+# run in the command line
+module load bioinfo-tools
+module load MUMmer
 
-/usr/local/MUMmer3.23/mummer -mum -b -c /home/caromoit/Desktop/sel2_NW_015504334.fna.gz //home/caromoit/Desktop/output_gapcloser.fasta /home/caromoit/Desktop/scaffolds.fasta > /home/caromoit/Desktop/output_mummer.mums
+# to run mummer
+mummer -mum -b -c /home/carolpb/GenomeAnalysis/data/wgs_from_paper/sel2_NW_015504334_unziped.fasta /home/carolpb/GenomeAnalysis/results/4_GapCloser_dna/output_gapcloser.fasta /home/carolpb/GenomeAnalysis/results/3_Assembly_spades/scaffolds.fasta > /home/carolpb/GenomeAnalysis/results/5_Result_Mummer/output_mummer.mums
+
+# to run mummerplot
+mummerplot -postscript -p mummer output_mummer.mums
+
+# to run nucmer soap
+nucmer --prefix=nucmer_soap /home/carolpb/GenomeAnalysis/data/wgs_from_paper/sel2_NW_015504334_unziped.fasta /home/carolpb/GenomeAnalysis/results/4_GapCloser_dna/output_gapcloser.fasta
+
+nucmer --prefix=ref_qry ref.fasta qry.fasta
+
+show-coords -rcl nucmer_soap.delta > nucmer_soap.coords
+
+show-aligns nucmer_soap.delta refname nucmer_soap > nucmer_soap.aligns
+
+show-tiling ref_qry.delta > ref_qry.tiling
+
+
+# to run nucmer spades 
+nucmer --prefix=nucmer_spades /home/carolpb/GenomeAnalysis/data/wgs_from_paper/sel2_NW_015504334_unziped.fasta /home/carolpb/GenomeAnalysis/results/3_Assembly_spades/scaffolds.fasta
+
+
