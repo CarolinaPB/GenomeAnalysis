aa1efe81dcf16bf9630c6966573179f9e30cc60f
CarolinaPB <35965407+CarolinaPB@users.noreply.github.com>
2019-05-03 15:12:24 +0200

    added lines to run Nucmer and mummer plot

--- code/mummer.sh
+++ code/mummer.sh
@@ -8,17 +8,14 @@ mummer -mum -b -c /home/carolpb/GenomeAnalysis/data/wgs_from_paper/sel2_NW_01550
 # to run mummerplot
 mummerplot -postscript -p mummer output_mummer.mums
 
+## NUCMER
 # to run nucmer soap
 nucmer --prefix=nucmer_soap /home/carolpb/GenomeAnalysis/data/wgs_from_paper/sel2_NW_015504334_unziped.fasta /home/carolpb/GenomeAnalysis/results/4_GapCloser_dna/output_gapcloser.fasta
-
-nucmer --prefix=ref_qry ref.fasta qry.fasta
-
-show-coords -rcl nucmer_soap.delta > nucmer_soap.coords
-
-show-aligns nucmer_soap.delta refname nucmer_soap > nucmer_soap.aligns
-
-show-tiling ref_qry.delta > ref_qry.tiling
+# to plot results from nucmer
+mummerplot -x "[0,2500000]" -y "[0,3000000]" --png --layout --filter -p soapdenovo_mummer /home/carolpb/GenomeAnalysis/results/5_Result_nucmer/nucmer_soap.delta -R //home/carolpb/GenomeAnalysis/data/wgs_from_paper/sel2_NW_015504334_unziped.fasta -Q /home/carolpb/GenomeAnalysis/results/4_GapCloser_dna/output_gapcloser.fasta
 
 
 # to run nucmer spades
 nucmer --prefix=nucmer_spades /home/carolpb/GenomeAnalysis/data/wgs_from_paper/sel2_NW_015504334_unziped.fasta /home/carolpb/GenomeAnalysis/results/3_Assembly_spades/scaffolds.fasta
+# to plot results from nucmer
+mummerplot -x "[0,2500000]" -y "[0,2300000]" --png --layout --filter -p spades_mummer /home/carolpb/GenomeAnalysis/results/5_Result_nucmer/nucmer_spades.delta -R //home/carolpb/GenomeAnalysis/data/wgs_from_paper/sel2_NW_015504334_unziped.fasta -Q /home/carolpb/GenomeAnalysis/results/3_Assembly_spades/scaffolds.fasta
