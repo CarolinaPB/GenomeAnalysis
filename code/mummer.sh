28ec2bb3e7b935a2babdef879b87ed363691ad25
CarolinaPB <carolina.pitabarros@gmail.com>
2019-05-03 15:04:54 +0200

    changes to mummer

--- code/mummer.sh
+++ code/mummer.sh
@@ -1,3 +1,3 @@
 #Run locally on the lab computers
 
-/usr/local/MUMmer3.23/mummer -mum -b -c /home/caromoit/Desktop/sel2_NW_015504334.fna.gz //home/caromoit/Desktop/output_gapcloser.fasta /home/caromoit/Desktop/scaffolds.fasta > /home/caromoit/Desktop/output_mummer.mums
+/usr/local/MUMmer3.23/mummer -mum -b -c /home/caromoit/Desktop/sel2_NW_015504334.fna.gz /home/caromoit/Desktop/output_gapcloser.fasta /home/caromoit/Desktop/scaffolds.fasta > /home/caromoit/Desktop/output_mummer.mums
