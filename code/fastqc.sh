82822490282a7978a7260ec1b289ae070741e28c
CarolinaPB <35965407+CarolinaPB@users.noreply.github.com>
2019-05-08 16:37:48 +0200

    updated output path

--- code/fastqc.sh
+++ code/fastqc.sh
@@ -5,12 +5,12 @@
 #SBATCH -t 00:05:00
 #SBATCH -J run_fastqc
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user carolina.pitabarros@gmail.com
+#SBATCH --mail-user Carolina.Barros.8371@student.uu.se
 # Load modules
 module load bioinfo-tools
 module load FastQC/0.11.5
 # Your commands
 for filename in /domus/h1/carolpb/GenomeAnalysis/data/wgs/*.fastq.gz
     do
-        fastqc $filename -o /home/carolpb/GenomeAnalysis/data/wgs/fastqc_results;
+        fastqc $filename -o /home/carolpb/GenomeAnalysis/results/1_QC_dna/fastqc_results;
     done
