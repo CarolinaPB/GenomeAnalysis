60a04f359ff184dc5ccd0e80c69f105579ca013f
CarolinaPB <carolina.pitabarros@gmail.com>
2019-05-15 17:16:23 +0200

    script to run bowtie

---
+++ code/bowtie.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -l
+#SBATCH -A g2019003
+#SBATCH -p core
+#SBATCH -n 4
+#SBATCH -t 02:30:00
+#SBATCH -J bowtie
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user Carolina.Barros.8371@student.uu.se
+# Load modules
+module load bioinfo-tools
+module load tophat
+
+bowtie2-build -c [/home/carolpb/GenomeAnalysis/data/wgs_from_paper/sel2_NW_015504334_unziped.fasta] /home/carolpb/GenomeAnalysis/results/7_bowtie/ref
