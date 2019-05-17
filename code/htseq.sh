9faa52276c19b3c0b7f34a370db37663b4a6e57a
CarolinaPB <carolina.pitabarros@gmail.com>
2019-05-17 10:56:26 +0200

    Update htseq.sh

--- code/htseq.sh
+++ code/htseq.sh
@@ -14,4 +14,4 @@ module load htseq
 # remove the fasta part and create a new file:
 head -24077 /home/carolpb/GenomeAnalysis/results/6_Maker2_paper/run_1/sel2_NW_015504334_unziped.maker.output/sel2_NW_015504334_unziped.all.gff > /home/carolpb/GenomeAnalysis/results/6_Maker2_paper/run_1/sel2_NW_015504334_unziped.maker.output/sel2_NW_015504334_unziped_nofasta.all.gff
 
-htseq-count -f bam -s no -t gene -i ID /home/carolpb/GenomeAnalysis/results/8_tophat/accepted_hits.bam /home/carolpb/GenomeAnalysis/results/6_Maker2_paper/run_1/sel2_NW_015504334_unziped.maker.output/sel2_NW_015504334_unziped_nofasta.all.gff
+htseq-count -f bam -s no -t gene -i ID /home/carolpb/GenomeAnalysis/results/8_tophat/accepted_hits.bam /home/carolpb/GenomeAnalysis/results/6_Maker2_paper/run_1/sel2_NW_015504334_unziped.maker.output/sel2_NW_015504334_unziped_nofasta.all.gff > htseq_results.txt
